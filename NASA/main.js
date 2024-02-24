de76e03febfee8f7aee9ccfc87642b0806722272
angelesGladin <160809137+angelesGladin@users.noreply.github.com>
2024-02-23 22:54:26 -0600

    MariaMorales-NASA
    Ejercicio de pagina web con HTML, CSS, Bootstrap y JS

---
+++ NASA/main.js
@@ -0,0 +1,14 @@
+function mostrar(){
+    document.getElementById('contenedor').style.display = 'block';
+    document.getElementById('contenedor').style.display = 'flex';
+    document.getElementById('contenedor').style.flexDirection ='row';
+    document.getElementById('contenedor2').style.display = 'block';
+    document.getElementById('contenedor2').style.display = 'flex';
+    document.getElementById('contenedor2').style.flexDirection ='row';
+}
+
+function ocultar(){
+    document.getElementById('contenedor').style.display = 'none';
+    document.getElementById('contenedor2').style.display = 'none';
+}
+  
